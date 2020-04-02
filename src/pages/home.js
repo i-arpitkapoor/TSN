3a98b4ab45ccaafc0844be8e2e555c79120713ea
Arpit Kapoor <i-arpitkapoor>
2020-04-02 18:39:21 +0530

    init commit

---
+++ src/pages/home.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
+import axios from 'axios'
+
+import Scream from '../components/Scream'
+
+class home extends Component {
+    state = {
+        screams: null
+    }
+    componentDidMount() {
+        axios.get('/screams')
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    screams: res.data
+                })
+            })
+            .catch(err => console.log(err))
+    }
+    render() {
+        let recentScreamsMarkup = this.state.screams ? (
+            this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
+        ) : (
+                <p> Loading ...</p>
+            )
+        return (
+            <Grid container spacing={16}>
+                <Grid container item sm={8} xs={12}>
+                    {recentScreamsMarkup}
+                </Grid>
+                <Grid container item sm={4} xs={12}>
+                    <p>Profile...</p>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default home
