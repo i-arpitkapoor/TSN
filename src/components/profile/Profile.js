b673f5200d1724da50e9f4a7b14b48ced4c667f6
Arpit Kapoor <i-arpitkapoor>
2020-04-08 21:27:11 +0530

    added skeleton

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom'
 import dayjs from 'dayjs'
 import EditDetails from './EditDetails'
 import MyButton from '../../util/MyButton'
+
+import ProfileSkeleton from '../../util/ProfileSkeleton'
+
 //redux
 import { connect } from 'react-redux'
 import { logoutUser, uploadImage } from '../../redux/actions/userActions'
@@ -25,51 +28,7 @@ import EditIcon from '@material-ui/icons/Edit'
 import KeyboardReturn from '@material-ui/icons/KeyboardReturn'
 
 const styles = (theme) => ({
-    paper: {
-        padding: 20
-    },
-    profile: {
-        '& .image-wrapper': {
-            textAlign: 'center',
-            position: 'relative',
-            '& button': {
-                position: 'absolute',
-                top: '80%',
-                left: '70%'
-            }
-        },
-        '& .profile-image': {
-            width: 200,
-            height: 200,
-            objectFit: 'cover',
-            maxWidth: '100%',
-            borderRadius: '50%'
-        },
-        '& .profile-details': {
-            textAlign: 'center',
-            '& span, svg': {
-                verticalAlign: 'middle'
-            },
-            '& a': {
-                color: theme.palette.primary.main
-            }
-        },
-        '& hr': {
-            border: 'none',
-            margin: '0 0 10px 0'
-        },
-        '& svg.button': {
-            '&:hover': {
-                cursor: 'pointer'
-            }
-        }
-    },
-    buttons: {
-        textAlign: 'center',
-        '& a': {
-            margin: '20px 10px'
-        }
-    }
+    ...theme.styles
 });
 
 class Profile extends Component {
@@ -160,7 +119,7 @@ class Profile extends Component {
                     </Button>
                     </div>
                 </Paper>
-            )) : (<p>loading...</p>)
+            )) : <ProfileSkeleton />
 
         return profileMarkup;
     }
